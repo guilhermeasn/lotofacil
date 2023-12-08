be5dab4f2f2a63fcb9fb94ee5162fc6359b38933
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-12-08 20:08:42 -0300

    generateCombinations improved

--- generateCombinations.ts
+++ generateCombinations.ts
@@ -2,15 +2,15 @@ function generateCombinations(amount : number, overall : number = 25) {
     
     let result : number[][] = [];
 
-    function combineUtil(start : number = 0, combination : number[] = []) {
+    function combineUtil(start : number = 1, combination : number[] = []) {
 
         if (combination.length === amount) {
             result.push([...combination]);
             return;
         }
 
-        for (let i = start; i < overall; i++) {
-            combination.push(i + 1);
+        for (let i = start; i <= overall; i++) {
+            combination.push(i);
             combineUtil(i + 1, combination);
             combination.pop();
         }
@@ -23,4 +23,5 @@ function generateCombinations(amount : number, overall : number = 25) {
     
 }
 
-console.log(generateCombinations(3).join(' | '));
+let count = 0;
+console.log(generateCombinations(3).map(raffle => `${++count} => ${raffle.join(' ')}`).join('\n'));
