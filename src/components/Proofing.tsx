9a8287f62551c1915cc29996994edd649cd94478
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-12-19 21:22:15 -0300

    first version

--- src/components/Proofing.tsx
+++ src/components/Proofing.tsx
@@ -1,5 +1,6 @@
 import map from "object-as-array/map";
 import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 import { Bet } from "../App";
 import { Raffles, raffles } from "../helpers/fetch";
 import { matches, mean, numberOfCombination, pairs, primes, sum } from "../helpers/math";
@@ -21,10 +22,10 @@ type Analytic = {
 export default function Proofing({ bets } : ProofingProps) {
 
     const [ data, setData ] = useState<Raffles>(null);
-    const [ analytic, setAnalytic ] = useState<Array<Analytic | null>>([]);
+    const [ analytics, setAnalytics ] = useState<Array<Analytic | null>>([]);
 
     useEffect(() => {
-        if(data) setAnalytic(bets.map<Analytic | null>(bet => bet.valid ? ({
+        if(data) setAnalytics(bets.map<Analytic | null>(bet => bet.valid ? ({
             relative : numberOfCombination(bet.balls, 25),
             sum      : sum(bet.balls),
             mean     : mean(bet.balls),
@@ -34,9 +35,51 @@ export default function Proofing({ bets } : ProofingProps) {
         }) : null)); else raffles().then(setData);
     }, [ data, bets ]);
 
+    return !data ? <Loading /> : (
 
-    console.log(analytic);
+        <Table variant="warning" striped hover responsive bordered>
 
-    return data ? <></> : <Loading />;
+            <thead>
+                <tr className="text-center">
+                    <td>#</td>
+                    <th>Número&nbsp;Relativo</th>
+                    <th>Somatório</th>
+                    <th>Média</th>
+                    <th>Números&nbsp;Pares</th>
+                    <th>Números&nbsp;Primos</th>
+                    { Array(11).fill(5).map((v, k) => <th key={ k }>{ k + v }&nbsp;Acertos</th>) }
+                </tr>
+            </thead>
+
+            <tbody>
+                { analytics.map((analytic, index) => (
+                    <tr key={ index }>
+                        <th>{ index + 1 }</th>
+
+                        { analytic === null ? <td className="text-danger" colSpan={ 16 }>Aposta inválida! Verifique!</td> : <>
+                        
+                            <td>{ analytic.relative.toLocaleString('pt-BR') }</td>
+                            <td>{ analytic.sum }</td>
+                            <td>{ analytic.mean }</td>
+                            <td>{ analytic.pairs }</td>
+                            <td>{ analytic.primes }</td>
+                            { Array(11).fill(5).map((v, k) => <td key={ k }>{ analytic.hits?.[k + v] ?? 0 }</td>) }
+
+                        </> }
+
+                    </tr>
+                )) }
+            </tbody>
+
+            <tfoot>
+                <tr>
+                    <th className="text-center" colSpan={ 6 }>TOTAL</th>
+                    { Array(11).fill(5).map((v, k) => <th key={ k }>{ analytics.reduce((sum, analytic) => sum + (analytic?.hits?.[k + v] ?? 0), 0) }</th>) }
+                </tr>
+            </tfoot>
+            
+        </Table>
+
+    );
 
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './helpers/loading.css';
 import './helpers/style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-    <React.StrictMode>
+createRoot(
+    document.getElementById('root') as HTMLElement
+).render(
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 );
