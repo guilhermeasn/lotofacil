3cd189167d30c55654af4d16a588ea75d5db34db
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-12-24 00:07:30 -0300

    modal generator random

--- src/components/ModalGenerator.tsx
+++ src/components/ModalGenerator.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Modal, Tab, Tabs } from "react-bootstrap";
+import { Button, FormCheck, FormGroup, FormLabel, FormSelect, Modal, Spinner, Tab, Tabs } from "react-bootstrap";
 import { surprise } from "../helpers/math";
 import Range from "./Range";
 
@@ -13,13 +13,24 @@ export default function ModalGenerator({ show, onHide, onMake } : ModalGenerator
 
     const [ balls, setBalls ] = useState<number>(15);
     const [ tab, setTab ] = useState<'random' | 'smart'>('random');
+    const [ wait, setWait ] = useState<boolean>(false);
+
     const [ randoms, setRandoms ] = useState<number>(1);
 
+    const [ hits, setHits ] = useState<number[]>([ 13, 14 ]);
+
     const getRandoms = () => {
+
+        setWait(true);
+
         const bets : number[][] = [];
         while(bets.length < randoms) bets.push(surprise(balls, 25));
+
         onMake(bets);
         onHide();
+
+        setTimeout(() => setWait(false), 1000);
+
     }
 
     const getSmarts = () => {
@@ -28,9 +39,9 @@ export default function ModalGenerator({ show, onHide, onMake } : ModalGenerator
 
     return (
 
-        <Modal show={ show } onHide={ onHide }>
+        <Modal show={ show } onHide={ wait ? undefined : onHide }>
 
-            <Modal.Header className="alert alert-success rounded-bottom-0 user-select-none" closeButton>
+            <Modal.Header className="alert alert-success rounded-bottom-0 user-select-none" closeButton={ !wait }>
                 <Modal.Title>Gerador</Modal.Title>
             </Modal.Header>
 
@@ -58,7 +69,43 @@ export default function ModalGenerator({ show, onHide, onMake } : ModalGenerator
                     </Tab>
 
                     <Tab className="p-4" eventKey="smart" title="Modo Inteligente">
-                        Tab content for Profile
+
+                        <FormGroup>
+
+                            <FormLabel>Obteve mais acertos com:</FormLabel>
+
+                            <div className="text-center">
+                                { Array(5).fill(11).map((v, k) => (
+                                    <FormCheck
+                                        key={ k }
+                                        label={ k + v }
+                                        type="checkbox"
+                                        checked={ hits.some(hit => hit === k + v) }
+                                        onChange={ () => setHits(hits => hits.some(hit => hit === k + v) ? hits.filter(hit => hit !== k + v) : [ ...hits, k + v ]) }
+                                        inline
+                                    />
+                                )) }
+                            </div>
+
+                        </FormGroup>
+
+                        <FormGroup className="mt-3">
+
+                            <FormLabel>Dos sorteios:</FormLabel>
+
+                            <FormSelect>
+                                <option value="30">últimos trinta sorteios</option>
+                                <option value="24">últimos vinte e quatro sorteios</option>
+                                <option value="18">últimos dezoito sorteios</option>
+                                <option value="12">últimos doze sorteios</option>
+                                <option value="6">últimos seis sorteios</option>
+                                <option value="3">últimos três sorteios</option>
+                                <option value="1">último sorteio</option>
+                                <option value="0">todos os sorteios</option>
+                            </FormSelect>
+
+                        </FormGroup>
+
                     </Tab>
 
                 </Tabs>
@@ -71,8 +118,8 @@ export default function ModalGenerator({ show, onHide, onMake } : ModalGenerator
                     Cancelar
                 </Button>
 
-                <Button variant="outline-success" onClick={ tab === 'random' ? getRandoms : getSmarts }>
-                    Gerar
+                <Button variant="outline-success" onClick={ tab === 'random' ? getRandoms : getSmarts } disabled={ wait || hits.length < 1 }>
+                    { wait ? <Spinner size='sm' /> : 'Gerar' }
                 </Button>
 
             </Modal.Footer>
